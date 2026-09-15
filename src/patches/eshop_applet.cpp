927daa11ba66de6846d8994e3e1835f3c862487c
rasberrytech <reedx33@gmail.com>
2026-09-14 17:11:07 -0700

    fix eshop patch

--- src/patches/eshop_applet.cpp
+++ src/patches/eshop_applet.cpp
@@ -62,11 +62,6 @@ DECL_FUNCTION(int, FSOpenFile_eShop, FSClient *client, FSCmdBlock *block, char *
               int error) {
     const char *initialOma = "vol/content/initial.oma";
 
-    if (!Config::connect_to_network) {
-        DEBUG_FUNCTION_LINE_VERBOSE("Inkay: eShop patches skipped.");
-        return real_FSOpenFile_eShop(client, block, path, mode, handle, error);
-    }
-
     if (strcmp(initialOma, path) == 0) {
         //below is a hacky (yet functional!) way to get Inkay to redirect URLs from the Miiverse applet
         //we do it when loading this file since it should only load once, preventing massive lag spikes as it searches all of MEM2 xD
@@ -78,38 +73,6 @@ DECL_FUNCTION(int, FSOpenFile_eShop, FSClient *client, FSCmdBlock *block, char *
 
         if (!replace(0x10000000, 0x10000000, (const char *)&original_entry, sizeof(original_entry), (const char *)&new_entry, sizeof(new_entry)))
             DEBUG_FUNCTION_LINE_VERBOSE("Inkay: We didn't find the whitelist /)>~<(\\");
-
-    // Check for root CA file and take note of its handle
-    } else if (strcmp("vol/content/browser/rootca.pem", path) == 0) {
-        int ret = real_FSOpenFile_eShop(client, block, path, mode, handle, error);
-        rootca_pem_handle = *handle;
-        DEBUG_FUNCTION_LINE_VERBOSE("Inkay: Found eShop CA, replacing...");
-        return ret;
-    }
-
-    return real_FSOpenFile_eShop(client, block, path, mode, handle, error);
-}
-
-DECL_FUNCTION(FSStatus, FSReadFile_eShop, FSClient *client, FSCmdBlock *block, uint8_t *buffer, uint32_t size, uint32_t count,
-              FSFileHandle handle, uint32_t unk1, uint32_t flags) {
-    if (size != 1) {
-        DEBUG_FUNCTION_LINE("Inkay: eShop CA replacement failed!");
-    }
-
-    if (rootca_pem_handle && *rootca_pem_handle == handle) {
-        strlcpy((char *) buffer, (const char *) ca_pem, size * count);
-        return (FSStatus) count;
-    }
-
-    return real_FSReadFile_eShop(client, block, buffer, size, count, handle, unk1, flags);
-}
-
-DECL_FUNCTION(FSStatus, FSCloseFile_eShop, FSClient *client, FSCmdBlock *block, FSFileHandle handle, FSErrorFlag errorMask) {
-    if (handle == rootca_pem_handle) {
-        rootca_pem_handle.reset();
-    }
-
-    return real_FSCloseFile_eShop(client, block, handle, errorMask);
 }
 
 void patchEshop() {
